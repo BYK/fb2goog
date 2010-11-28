e72e5421db0982b9be75f352ea3f5524b3f29679
Berker Peksag <berker.peksag@gmail.com>
2010-11-28 17:57:57 +0200

    Initial UI.

--- home.py
+++ home.py
@@ -3,6 +3,8 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import db
 
 import os
 import sys
@@ -13,9 +15,24 @@ sys.setrecursionlimit(10000) # SDK fix
 class MainPage(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
+
+        user = users.get_current_user()
+
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            is_logged = True
+        else:
+            url = users.create_login_url(self.request.uri)
+            is_logged = False
+
+        values = {
+            'user': user,            
+            'url': url,
+            'is_logged': is_logged,
+        }
         
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
-        self.response.out.write(template.render(path, None))
+        self.response.out.write(template.render(path, values))
 
     def put(self):
         pass
